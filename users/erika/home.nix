c00e06735caf4406883d76b5899adf930d988ed0
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-05-14 18:57:02 -0400

    add erika's home profile

---
+++ users/erika/home.nix
@@ -0,0 +1,60 @@
+{ pkgs, configRoot, ... }:
+
+{
+	home.username = "erika";
+	home.homeDirectory = "/home/erika";
+	home.stateVersion = "23.11";
+	programs = {
+		home-manager.enable = true;
+		vscode.enable = true;
+		zathura.enable = true;
+		chromium.enable = true;
+		firefox.enable = true;
+	};
+	home.packages = with pkgs; [
+		zsh
+		#shell utilities
+		ranger
+		btop
+		# nvim dependencies
+		gnumake
+		gcc
+		antidote
+		fd
+		ripgrep
+		git
+		neovide
+		xclip
+		jdk
+		# LSPs
+		lua-language-server
+		jdt-language-server
+		taplo
+		nodePackages_latest.pyright
+		ruff-lsp
+		texlab
+		nil
+		# tex
+		texliveFull
+		# python
+		# TODO: create shell.nix to run FHS for this
+		micromamba
+		# desktop apps
+		xournalpp
+		gimp
+		discord
+		slack
+		libreoffice
+		thunderbird
+	];
+	home.file = {
+		".config/zsh/zshrc".source = "${configRoot}/dotfiles/zsh/zshrc";
+		".config/zsh/.zshrc".text  = ''
+			source ${pkgs.antidote}/share/antidote/antidote.zsh
+			source ''${HOME}/.config/zsh/zshrc
+		'';
+		".config/zsh/p10k.zsh".source = "${configRoot}/dotfiles/zsh/p10k.zsh";
+		".config/zsh/zsh_plugins.conf".source
+			= "${configRoot}/dotfiles/zsh/zsh_plugins.conf";
+	};
+}
