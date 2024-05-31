46b74140d2829d9b20de6b1150d63ff8421467f1
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-05-31 15:43:46 -0400

    Refactor hardware configuration and add gpu config

--- hosts/panza/hardware/sound.nix
+++ hosts/panza/hardware/sound.nix
@@ -1,12 +1,8 @@
 { ... }:
 
 {
-	#boot.kernelParams = ["i915.enable_guc=2"];
-	hardware.sensor.iio.enable = true;
-	services.fprintd.enable = true;
 	sound.enable = true;
 	hardware.pulseaudio.enable = false;
-	security.rtkit.enable = true;
 	services.pipewire = {
 		enable = true;
 		alsa.enable = true;
