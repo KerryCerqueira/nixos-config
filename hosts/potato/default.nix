7e2bef6c2c019dc43a508814332c8527b5e04697
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-06-07 15:35:00 -0400

    Refactor home and system configs
    Use shared functional modules. Do secrets management per-machine now.

--- hosts/potato/default.nix
+++ hosts/potato/default.nix
@@ -2,71 +2,31 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ stateVersion, hostName, ... }:
+
 
 {
 	imports = [
-		./hardware-configuration.nix
+		./hardware
+		../common/core/systemd-boot.nix
+		../common/optional/gnome.nix
+		../common/optional/steam.nix
 	];
-	system.stateVersion = "23.11";
+	system.stateVersion = stateVersion;
 	nix.settings.experimental-features = ["nix-command" "flakes"];
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
-	boot.loader.systemd-boot.configurationLimit = 1;
-	networking.hostName = "potato";
+	nixpkgs.config.allowUnfree = true;
+	networking.hostName = hostName;
 	networking.networkmanager.enable = true;
 	time.timeZone = "America/Toronto";
 	i18n.defaultLocale = "en_CA.UTF-8";
-	services.xserver.enable = true;
-	services.xserver.displayManager.gdm.enable = true;
-	services.xserver.desktopManager.gnome.enable = true;
-	services.xserver.xkb = {
-		layout = "ca";
-		variant = "";
+	services.xserver = {
+		enable = true;
+		videoDrivers = ["nvidia"];
+		displayManager.gdm.enable = true;
+		xkb = {
+			layout = "us";
+			variant = "";
+		};
 	};
 	services.printing.enable = true;
-	sound.enable = true;
-	hardware.pulseaudio.enable = false;
-	security.rtkit.enable = true;
-	services.pipewire = {
-		enable = true;
-		alsa.enable = true;
-		alsa.support32Bit = true;
-		pulse.enable = true;
-	};
-	users.users = {
-		kerry = {
-			isNormalUser = true;
-			description = "Kerry";
-			extraGroups = [ "networkmanager" "wheel" ];
-		};
-		erika = {
-			isNormalUser = true;
-			description = "Erika";
-			extraGroups = [ "networkmanager" "wheel" ];
-		};
-	};
-
-	nixpkgs.config.allowUnfree = true;
-	environment.systemPackages = with pkgs; [
-		kitty
-		easyeffects
-	];
-	fonts.packages = with pkgs ; [
-		iosevka
-		(nerdfonts.override {fonts = ["Iosevka"];})
-	];
-	environment.variables = {
-		ZDOTDIR = "$HOME/.config/zsh";
-	};
-	users.defaultUserShell = pkgs.zsh;
-	programs = {
-		zsh.enable = true;
-		dconf.enable = true;
-		neovim = {
-			enable = true;
-			defaultEditor = true;
-		};
-		steam.enable = true;
-	};
 }
