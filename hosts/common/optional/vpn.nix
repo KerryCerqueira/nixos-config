757f37d672ba071755614d417a9bdf9bc8cd4ba6
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-08-06 15:42:36 -0400

    Change openvpn module to use globalprotect GUI

--- hosts/common/optional/vpn.nix
+++ hosts/common/optional/vpn.nix
@@ -1,11 +1,7 @@
-{ config, ... }:
+{ pkgs, ... }:
 
 {
-	services.openvpn.servers = {
-		bic = {
-			autoStart = false;
-			config = "config ${config.sops.secrets."vpnConfs/bic".path}";
-			updateResolvConf = true;
-		};
-	};
+	environment.systemPackages = with pkgs; [
+		globalprotect-openconnect
+	];
 }
