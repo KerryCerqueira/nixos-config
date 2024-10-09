a5be7d2929a64618c4cb2f19a0f93d7c9f61738d
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-10-09 19:41:21 -0400

    Apply catppuccin to gtk and qt. Add and configure kde dolphin, waybar
    gtk catpuccin theme is achieved with deprecated gtk-catpuccin package.
    Cross that bridge when we get to it. qt theme relies on settings from
    kvantum and qt6ct which also apply the icon and menu theme settings.
    Additional config files for these deployed with home manager. #40
    requires a few more applications to be themed in this way.
    
    Configure kde dolphin file manager and its dependencies, which include
    thumbnailers, compatibility packages for wayland, and compatibility
    packages to enable svg fonts. Include these in system hyprland module,
    which begs for a future refactor.
    
    Add starter waybar config with home manager deployment. Closes #34.

--- hosts/common/optional/hyprland.nix
+++ hosts/common/optional/hyprland.nix
@@ -4,14 +4,18 @@ let
 	# hyprland-pkgs = hyprland.inputs.nixpkgs.legacyPackages.${system};
 in
 {
-	nix.settings = {
-		substituters = ["https://hyprland.cachix.org"];
-		trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-	};
 	# hardware.graphics = {
 	# 	package = hyprland-pkgs.mesa.drivers;
 	# 	package32 = hyprland-pkgs.pkgsi686Linux.mesa.drivers;
 	# };
+	nix.settings = {
+		substituters = ["https://hyprland.cachix.org"];
+		trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+	};
+	environment.sessionVariables = {
+		# hint electron apps to use wayland
+		NIXOS_OZONE_WL = "1";
+	};
 	programs = {
 		hyprland = {
 			enable = true;
@@ -19,27 +23,60 @@ in
 			portalPackage = inputs.hyprland.packages.${system}.xdg-desktop-portal-hyprland;
 		};
 		iio-hyprland.enable = true;
-		hyprlock.enable = true;
 		nm-applet.enable = true;
+		hyprlock.enable = true;
+		ssh.startAgent = true;
+		dconf.enable = true;
 	};
 	services.blueman.enable = true;
+	services.udisks2.enable = true;
 	environment.systemPackages = with pkgs; [
 		# status bar
 		waybar
 		# notification daemon
 		swaynotificationcenter
+		# kde wayland support
+		kdePackages.qtwayland
 		# polkit agent
 		kdePackages.polkit-kde-agent-1
+		# file manager
+		kdePackages.dolphin
+		kdePackages.dolphin-plugins
+		# icon support for dolphin
+		kdePackages.qtsvg
+		# thumbnailers and file previews for dolphin
+		kdePackages.kdegraphics-thumbnailers
+		kdePackages.kimageformats
+		kdePackages.qtimageformats
+		kdePackages.kdesdk-thumbnailers
+		kdePackages.ffmpegthumbs
+		kdePackages.taglib
+		resvg
+		kdePackages.kio-fuse #to mount remote filesystems via FUSE
+		kdePackages.kio-extras #extra protocols support (sftp, fish and more)
+		kdePackages.kio-zeroconf
+		kdePackages.kio-gdrive
+		# terminal for dolphin
+		kdePackages.konsole
+		# compare tool for dolphin
+		kdePackages.kompare
+		# metadata search for dolphin
+		kdePackages.baloo
+		# file archiver
+		kdePackages.ark
 		# volume control
 		pavucontrol
+		# brightness control
+		brightnessctl
 		# display configuration
 		wlr-randr
 		kanshi
+		wdisplays
 		# launcher
 		rofi-wayland
 		# hypr plugins
-		hyprlock
 		hyprshot
-		hyprpaper
+		# clipboard for non windowed applications
+		wl-clipboard
 	];
 }
--- users/common/hyprland.nix
+++ users/common/hyprland.nix
@@ -1,11 +1,51 @@
 { pkgs, inputs, ... }:
 
 let
-	system = pkgs.system;
+	system = pkgs.stdenv.hostPlatform.system;
+	root = inputs.self;
 in
 {
-	services.hypridle.enable = true;
-	wayland.windowManager.hyprland.plugins = [
-		inputs.hyprspace.packages.${system}.Hyprspace
+	home.packages = with pkgs; [
+		papirus-icon-theme
+		qt6ct
 	];
+	imports = [ inputs.catppuccin.homeManagerModules.catppuccin ];
+	qt = {
+		enable = true;
+		platformTheme.name = "kvantum";
+		style.name = "kvantum";
+		style.catppuccin.enable = true;
+	};
+	gtk = {
+		enable = true;
+		#TODO: resolve this option if noto sans doesn't exist or add a mechanism to
+		# satisfy this dependency
+		font.name = "Noto Sans";
+		font.size = 10;
+		catppuccin.enable = true;
+		iconTheme = {
+			name = "Papirus-Dark";
+		};
+	};
+	home.file.".config/qt6ct/qt6ct.conf".source = "${root}/dotfiles/qt6ct/qt6ct.conf";
+	home.file.".config/kdeglobals".text = ''
+	[General]
+		TerminalApplication=kitty
+	'';
+	home.file.".config/waybar/" = {
+		source = "${root}/dotfiles/waybar";
+		recursive = true;
+	};
+	wayland.windowManager.hyprland = {
+		enable = true;
+		plugins = [
+			inputs.hyprspace.packages.${system}.Hyprspace
+		];
+		extraConfig = "source = ./config.d/general.conf";
+		package = inputs.hyprland.packages.${system}.hyprland;
+		catppuccin.enable = true;
+	};
+	catppuccin.pointerCursor.enable = true;
+	# services.hyprpaper.enable = true;
+	# services.hypridle.enable = true;
 }
