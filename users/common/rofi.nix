acc30a0e483a5fe67c2649f8839fed8a08942959
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-11-06 22:06:13 -0500

    Add rofi module
    Add rofi module which includes rofi plugins as per #58. Change waybar
    config to run rofi-top when clicking on cpu module. Switch from
    rofi-wayland to legacy rofi running under xWayland due to upstream
    issues. See #58 discussion.

--- users/common/rofi.nix
+++ users/common/rofi.nix
@@ -1,14 +1,21 @@
-{ pkgs, ... }:
-
-{
+{ inputs, config, pkgs, ... }:
+let
+	root = inputs.self;
+	inherit (config.lib.formats.rasi) mkLiteral;
+in {
 	programs.rofi = {
 		enable = true;
-		package = pkgs.rofi-wayland;
+		# package = pkgs.rofi-wayland;
 		plugins = with pkgs; [
 			rofi-calc
 			rofi-file-browser
-			rofi-power-menu
 			rofi-top
 		];
+	};
+	home.file = {
+		".config/rofi/conf.d" = {
+			source = "${root}/dotfiles/rofi/";
+			recursive = true;
+		};
 	};
 }
