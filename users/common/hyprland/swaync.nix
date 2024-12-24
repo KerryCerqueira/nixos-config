b351b633aa51db8e3bc47a18def05290c474800a
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-12-23 22:05:57 -0500

    Add battery module

--- users/common/hyprland/swaync.nix
+++ users/common/hyprland/swaync.nix
@@ -1,39 +1,12 @@
-
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 let
-	swayncConfig = {
-		"$schema" = "${pkgs.swaynotificationcenter}/etc/xdg/swaync/configSchema.json";
-		positionX = "center";
-		positionY = "top";
-		control-center-margin-top    = 10;
-		control-center-margin-bottom = 10;
-		control-center-margin-right  = 10;
-		control-center-margin-left   = 10;
-		widgets = [
-			"buttons-grid"
-			"dnd"
-			"title"
-			"notifications"
-		];
-		widget-config = {
-			buttons-grid.actions = [
-				{ label = "󰐥"; command = "systemctl poweroff"; }
-				{ label = "󰑐"; command = "systemctl reboot";   }
-				{ label = "󰌿"; command = "hyprlock";           }
-			];
-			dnd = {
-				text = "Do not disturb";
-			};
-			title = {
-				text = "Notifications";
-				clear-all-button = true;
-				button-text = "󰆴";
-			};    
-		};
-	}; 
+	root = inputs.self;
+	swayncConfig = (
+		builtins.fromJSON (builtins.readFile "${root}/dotfiles/swaync/config.json")
+		// {"$schema" = "${pkgs.swaynotificationcenter}/etc/xdg/swaync/configSchema.json";}
+	);
 in {
-
 	home = {
 		packages = [ pkgs.swaynotificationcenter ];
 		file = {
