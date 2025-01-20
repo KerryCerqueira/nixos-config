17d9c85ef70c29d711104a4e401de2acaaf80038
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-01-20 17:19:16 -0500

    Add github ssh keys to potato and lazarus kerry profile

--- users/kerry/hosts/lazarus/default.nix
+++ users/kerry/hosts/lazarus/default.nix
@@ -6,12 +6,20 @@
 		defaultSopsFormat = "yaml";
 		age.keyFile = "/home/kerry/.config/sops/age/kerry_lazarus.age";
 		secrets = {
-			"syncthing/cert" = {
-				path = "/home/kerry/.config/syncthing/cert.pem";
-			};
-			"syncthing/key" = {
-				path = "/home/kerry/.config/syncthing/key.pem";
-			};
+			"syncthing/cert".path = "/home/kerry/.config/syncthing/cert.pem";
+			"syncthing/key".path = "/home/kerry/.config/syncthing/key.pem";
+			"ssh/github/private".path = "/home/kerry/.ssh/github";
+		};
+	};
+	services.syncthing = {
+		enable = true;
+		tray.enable = true;
+	};
+	# See #87 for more info on this hack
+	systemd.user.targets.tray = {
+		Unit = {
+			Description = "Home Manager System Tray";
+			Requires = [ "graphical-session-pre.target" ];
 		};
 	};
 }
