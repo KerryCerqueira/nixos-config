34a56b6e0d0760b84671163975d661151dd466c9
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-07-27 00:31:10 -0400

    Update gnome module to support breaking change
    Add openconnect nm-manager plugin. Formatting.

--- hosts/common/gnome.nix
+++ hosts/common/gnome.nix
@@ -2,45 +2,54 @@
 
 {
 	powerManagement.enable = true;
-	services.desktopManager.gnome.enable = true;
-	services.xserver.excludePackages = [ pkgs.xterm ];
-	environment.gnome.excludePackages = with pkgs; [
-		orca
-		evince
-		geary
-		gnome-photos
-		gnome-tour
-		gnome-music
-		gnome-terminal
-		epiphany
-		gnome-text-editor
-		gnome-calendar
-		gnome-console
-		gnome-contacts
-		gnome-connections
-		gnome-music
-		totem
-	];
-	environment.systemPackages = (with pkgs; [
-		kitty
-	]) ++ (with pkgs.gnomeExtensions; [
-			appindicator
-			caffeine
-			clipboard-indicator
-			easyeffects-preset-selector
-			places-status-indicator
-			auto-move-windows
-			launch-new-instance
-			removable-drive-menu
-			vitals
-			quick-settings-tweaker
-			impatience
-			runcat
-		]);
-	services.udev.packages = with pkgs; [ gnome-settings-daemon ];
-	programs.dconf.enable = true;
-	programs.kdeconnect = {
-		enable = true;
-		package = pkgs.gnomeExtensions.gsconnect;
+	services = {
+		desktopManager.gnome.enable = true;
+		xserver.excludePackages = [ pkgs.xterm ];
+		udev.packages = with pkgs; [ gnome-settings-daemon ];
 	};
+	environment = {
+		gnome.excludePackages = with pkgs; [
+			orca
+			evince
+			geary
+			gnome-photos
+			gnome-tour
+			gnome-music
+			gnome-terminal
+			epiphany
+			gnome-text-editor
+			gnome-calendar
+			gnome-console
+			gnome-contacts
+			gnome-connections
+			gnome-music
+			totem
+		];
+		systemPackages = (with pkgs; [
+			kitty
+		]) ++ (with pkgs.gnomeExtensions; [
+				appindicator
+				caffeine
+				clipboard-indicator
+				easyeffects-preset-selector
+				places-status-indicator
+				auto-move-windows
+				launch-new-instance
+				removable-drive-menu
+				vitals
+				quick-settings-tweaker
+				impatience
+				runcat
+			]);
+	};
+	programs = {
+		dconf.enable = true;
+		kdeconnect = {
+			enable = true;
+			package = pkgs.gnomeExtensions.gsconnect;
+		};
+	};
+	networking.networkmanager.plugins = with pkgs; [
+		networkmanager-openconnect
+	];
 }
